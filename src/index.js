6c6509f61a25221dde5acabc8520cb0b9c87fcea
VivianaGuerraCustodio <vivianaguerracustodio@gmail.com>
2020-06-11 03:05:04 -0500

    add function getPropertiesOfMarkdownFiles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+/* eslint-disable no-undef */
+/* eslint-disable new-cap */
+/* eslint-disable import/no-unresolved */
 /* eslint-disable max-len */
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
-const http = require('http');
+// const http = require('http');
 const path = require('path');
 const fs = require('fs');
+const marked = require('marked');
 const jsdom = require('jsdom');
+
+const { JSDOM } = jsdom;
 
 // validar si la ruta es válida o no (preguntar acerca de esto porque la ruta relativa no la valida)
 const validateRoute = (routeInput) => (fs.existsSync(routeInput) ? routeInput : 'The input route is not valid');
@@ -18,7 +24,7 @@ console.log(convertPath('mkdir ~/Fotos'));
 const isDirectory = (routeInput) => (fs.statSync(routeInput).isDirectory()); // ? true : 'Is not a directory ');
 console.log(isDirectory('/home/ena/Desktop'));
 // lee el contenido del directorio
-const reviewDirectoryContent = (routeInput) => fs.readdirSync(routeInput);
+const reviewDirectoryContent = (routeInput) => fs.readdirSync(routeInput, 'utf-8');
 console.log(reviewDirectoryContent('/home/ena/Desktop/Proyectos de Laboratoria/LIM012-card-validation/'));
 // verifica si es un archivo
 const isFile = (routeInput) => (fs.statSync(routeInput).isFile());
@@ -26,6 +32,25 @@ console.log(isFile('/home/ena/Desktop/Proyectos de Laboratoria/LIM012-card-valid
 // ¿El archivo tiene extensión .md?
 const isAMarkdownFile = (routeInput) => (path.extname(routeInput) === '.md');
 console.log(isAMarkdownFile('/home/ena/Desktop/Proyectos de Laboratoria/LIM012-card-validation/README.css'));
+// revisa el archivo md y extrae las propiedades href, text, file y las guarda en un array
+// marked cambia el formato markdown a html
+// new jsdom es objeto constructor de simulacion del dom
+const getPropertiesOfMarkdownFile = (file) => {
+  const propertiesOfFile = [];
+  const readMarkdownFile = fs.readFileSync(file, 'utf-8');
+  const markdownToHtml = marked(readMarkdownFile);
+  const simulationDom = new JSDOM(markdownToHtml);
+  simulationDom.window.document.querySelectorAll('a').forEach((nodeElement) => {
+    propertiesOfFile.push({
+      href: nodeElement.getAttribute('href'),
+      text: nodeElement.textContent,
+      path: file,
+    });
+  });
+  return propertiesOfFile;
+};
+
+console.log(getPropertiesOfMarkdownFile('/home/ena/Desktop/Proyectos de Laboratoria/LIM012-fe-md-links/README.md'));
 
 module.exports = {
   validateRoute,
@@ -34,4 +59,5 @@ module.exports = {
   isFile,
   reviewDirectoryContent,
   isAMarkdownFile,
+  getPropertiesOfMarkdownFile,
 };
