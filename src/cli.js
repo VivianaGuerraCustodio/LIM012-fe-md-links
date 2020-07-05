4440eda5379cceb88ff529bffd929ac762d53dc5
VivianaGuerraCustodio <vivianaguerracustodio@gmail.com>
2020-07-05 11:35:21 -0500

    importing options module

--- src/cli.js
+++ src/cli.js
@@ -4,30 +4,11 @@
 /* eslint-disable no-console */
 
 const { mdLinks } = require('./mdLinks');
-
-const optionStats = (linksArr) => {
-  const totalLinks = linksArr.length;
-  const uniqueLinks = [...new Set(linksArr.map((links) => links.href))].length;
-  return { total: totalLinks, unique: uniqueLinks };
-};
-const statsAndValidate = (linksArr) => {
-  const totalLinks = linksArr.length;
-  const uniqueLinks = [...new Set(linksArr.map((links) => links.href))].length;
-  const brokenLinks = linksArr.filter((element) => element.status >= 400).length;
-  return { total: totalLinks, unique: uniqueLinks, broken: brokenLinks };
-};
-const help = `
-Questions❓
-Check this little guide 😊 ⬇️⬇️⬇️ 
-          mdLinks <path to file> [option]
-     ****************OPTIONS*****************
-     mdLinks <file path> --validate
-     mdLinks <file path> --stats
-     mdLinks <file path> --validate --stats
-     ****************************************
-If you did not find the help you were looking for, contact me at email   
-vivianaguerracustodio@gmail.com  
-     `;
+const {
+  optionStats,
+  statsAndValidate,
+  help,
+} = require('./options');
 
 const argvs = process.argv;
 const path = argvs[2];
